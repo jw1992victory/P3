47e6662a6ba1a7b92f976a1ca6e8622a0ab53804
wendy1992 <jw1992victory>
2016-10-23 19:28:53 -0400

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomePageController@index')->name('HomePage.index');
 
 Route::get('homepage', 'HomePageController@index')->name('HomePage.index');
 
